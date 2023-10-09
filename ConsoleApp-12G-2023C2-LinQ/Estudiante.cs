0ed75fbb8fb8bb24ffe93013515ec09e443cc574
christian.slapak <christian.slapak@ort.edu.ar>
2023-10-09 20:29:29 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp-12G-2023C2-LinQ/Estudiante.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp_12G_2023C2_LinQ
+{
+    class Estudiante
+    {
+        public int Id  { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
---
+++ ConsoleApp-12G-2023C2-LinQ/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp_12G_2023C2_LinQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Estudiante[] misEstudiantes =
+            {
+                new Estudiante() {Id = 1, Nombre = "Magadalena", Edad = 18},
+                new Estudiante() {Id = 2, Nombre = "Jorge", Edad = 29},
+                new Estudiante() {Id = 3, Nombre = "Fernanda", Edad = 13},
+                new Estudiante() {Id = 4, Nombre = "Javier", Edad = 17},
+                new Estudiante() {Id = 5, Nombre = "Cecila", Edad = 22},
+                new Estudiante() {Id = 6, Nombre = "Yamila", Edad = 31},
+                new Estudiante() {Id = 7, Nombre = "Federico", Edad = 25},
+                new Estudiante() {Id = 8, Nombre = "Miguel Angel", Edad = 34},
+            };
+
+            Estudiante[] estudiantesConsulta = new Estudiante[8];
+            int x = 0;
+            foreach (var item in misEstudiantes)
+            {
+                if (item.Edad >= 13 && item.Edad <= 21 ) //Filtrar
+                {
+                    estudiantesConsulta[x] = item;
+                    x++;
+                }
+            }
+
+            Console.WriteLine("Alumnos por bloque de codigo");
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine(estudiantesConsulta[i].Nombre.ToString() + " " + estudiantesConsulta[i].Edad);
+            }
+
+
+            Console.ReadKey();
+            //LinQ - Expresion de Consulta
+            Console.WriteLine("Alumnos por Expresion de Consulta con LinQ");
+
+            var miConsultaLinQEC = from itemEstudiante in misEstudiantes where itemEstudiante.Edad >= 13 && itemEstudiante.Edad <= 21 orderby itemEstudiante.Nombre select itemEstudiante;
+            foreach (var pepe in miConsultaLinQEC)
+            {
+                Console.WriteLine(pepe.Nombre + " " + pepe.Edad);
+            }
+            Console.ReadKey();
+
+            //LinQ -Expresion Lambda
+            Console.WriteLine("Alumnos por Expresion lambda con LinQ");
+            var miConsultaLinQL = misEstudiantes.Where(itemEstu => itemEstu.Edad >= 13 && itemEstu.Edad <= 21).ToArray();
+            foreach (var item in miConsultaLinQL)
+            {
+
+                Console.WriteLine(item.Nombre + " " + item.Edad);
+            }
+            Console.ReadKey();
+
+            Console.WriteLine("Alumnos por Expresion de Consulta 2 con LinQ");
+            var miConsultaLinQEC2 = from itemEstudiante in misEstudiantes where itemEstudiante.Nombre.StartsWith('F') orderby itemEstudiante.Edad select itemEstudiante;
+            foreach (var pepe in miConsultaLinQEC2)
+            {
+                Console.WriteLine(pepe.Nombre + " " + pepe.Edad);
+            }
+            Console.ReadKey();
+
+            Console.WriteLine("Traer uno");
+            Estudiante unEstudiante = misEstudiantes.Where(itemEstu => itemEstu.Id == 5).FirstOrDefault();
+            Console.WriteLine("Estudiante por Id " + unEstudiante.Nombre + " " + unEstudiante.Edad);
+
+            Console.ReadKey();
+
+        }
+    }
+}
